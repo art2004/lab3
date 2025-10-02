d0a2b9a2ddeb1edf559424814189f2fa3e26da1e
art2004 <kremenkov.artem@mail.ru>
2025-10-02 19:25:34 +0300

    5 Zadanie

--- lab3.py
+++ lab3.py
@@ -1,14 +1,14 @@
-# Создайте список квадратов этих чисел, используя list comprehension
+# 1. Создайте список квадратов этих чисел, используя list comprehension
 squares = [x ** 2 for x in range(1, 11)]
 # print(squares)
-# Список чётных
+# 2. Список чётных
 even_numbers = [x for x in range(1, 20) if x % 2 == 0]
 # print(even_numbers)
-# Создайте новый список, где все слова будут в верхнем регистре и длинее 3 символов.
+# 3. Создайте новый список, где все слова будут в верхнем регистре и длинее 3 символов.
 words = ["python", "Java", "c++", "Rust", "go"]
 upper_long3_words = [word.upper() for word in words if len(word) > 3]
 # print(upper_long3_words)
-# Создайте класс-итератор Countdown, который принимает число и при итерации возвращает числа от этого числа до 1
+# 4. Создайте класс-итератор Countdown, который принимает число и при итерации возвращает числа от этого числа до 1
 class Countdown:
     def __init__(self, start):
         self.current = start
@@ -23,5 +23,26 @@ class Countdown:
         self.current -= 1
         return result
 
-for x in Countdown(30):
-    print(x)
+#for x in Countdown(30):
+    #print(x)
+
+# 5. Напишите итератор fibonacci(n), который возвращает первые n чисел Фибоначчи.
+class Fibonacci:
+    def __init__(self, n):
+        self.n = n
+        self.count = 0
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= self.n:
+            raise StopIteration
+        result = self.a
+        self.a, self.b = self.b, self.a + self.b
+        self.count += 1
+        return result
+
+for num in Fibonacci(7):
+    print(num)
