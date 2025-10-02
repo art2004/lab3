b7a5f8cbdf9a614580774df596ac2bf525c37cc0
art2004 <kremenkov.artem@mail.ru>
2025-10-02 19:20:14 +0300

    3 Zadanie

--- lab3.py
+++ lab3.py
@@ -3,4 +3,8 @@ squares = [x ** 2 for x in range(1, 11)]
 # print(squares)
 # Список чётных
 even_numbers = [x for x in range(1, 20) if x % 2 == 0]
-print(even_numbers)
+# print(even_numbers)
+# Создайте новый список, где все слова будут в верхнем регистре и длинее 3 символов.
+words = ["python", "Java", "c++", "Rust", "go"]
+upper_long3_words = [word.upper() for word in words if len(word) > 3]
+print(upper_long3_words)
