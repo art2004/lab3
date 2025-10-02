49c5a6dade3419649c13a8e9516452926567f9d3
art2004 <kremenkov.artem@mail.ru>
2025-10-02 19:17:57 +0300

    2 Zadanie

--- lab3.py
+++ lab3.py
@@ -1,3 +1,6 @@
 # Создайте список квадратов этих чисел, используя list comprehension
 squares = [x ** 2 for x in range(1, 11)]
-print(squares)
+# print(squares)
+# Список чётных
+even_numbers = [x for x in range(1, 20) if x % 2 == 0]
+print(even_numbers)
